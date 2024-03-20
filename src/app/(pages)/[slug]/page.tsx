b4e06056f56c771acc3212a495d26c0b202d4939
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-03-21 00:54:48 +0100

    .

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -21,6 +21,8 @@ import { Gutter } from '../../_components/Gutter'
 export const dynamic = 'force-dynamic'
 
 import classes from './index.module.scss'
+import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -60,7 +62,10 @@ export default async function Page({ params: { slug = 'home' } }) {
       {slug === 'home' ? (
         <section>
           <Hero {...hero} />
-          <Gutter>{'kategorije'}</Gutter>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+            <Promotion />
+          </Gutter>
         </section>
       ) : (
         <>
