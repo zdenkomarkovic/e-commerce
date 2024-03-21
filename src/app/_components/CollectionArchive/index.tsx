1605363634be4e175514806ca463dc3de8d91427
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-03-21 02:10:46 +0100

    .

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -11,6 +11,7 @@ import { PageRange } from '../PageRange'
 import { Pagination } from '../Pagination'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../_providers/Filter'
 
 type Result = {
   docs: (Product | string)[]
@@ -38,8 +39,8 @@ export type Props = {
 }
 
 export const CollectionArchive: React.FC<Props> = props => {
+  const { categoryFilters, sort } = useFilter()
   const {
-    categories: catsFromProps,
     className,
     limit = 10,
     onResultChange,
@@ -49,7 +50,6 @@ export const CollectionArchive: React.FC<Props> = props => {
     relationTo,
     selectedDocs,
     showPageRange,
-    sort = '-createdAt',
   } = props
 
   const [results, setResults] = useState<Result>({
@@ -75,9 +75,9 @@ export const CollectionArchive: React.FC<Props> = props => {
   const isRequesting = useRef(false)
   const [page, setPage] = useState(1)
 
-  const categories = (catsFromProps || [])
-    .map(cat => (typeof cat === 'object' ? cat?.id : cat))
-    .join(',')
+  // const categories = (catsFromProps || [])
+  //   .map(cat => (typeof cat === 'object' ? cat?.id : cat))
+  //   .join(',')
 
   const scrollToRef = useCallback(() => {
     const { current } = scrollRef
@@ -116,10 +116,13 @@ export const CollectionArchive: React.FC<Props> = props => {
           page,
           sort,
           where: {
-            ...(categories
+            ...(categoryFilters && categoryFilters?.length > 0
               ? {
                   categories: {
-                    in: categories,
+                    in:
+                      typeof categoryFilters === 'string'
+                        ? [categoryFilters]
+                        : categoryFilters.map((cat: string) => cat).join(','),
                   },
                 }
               : {}),
@@ -162,7 +165,7 @@ export const CollectionArchive: React.FC<Props> = props => {
     return () => {
       if (timer) clearTimeout(timer)
     }
-  }, [page, categories, relationTo, onResultChange, sort, limit, populateBy])
+  }, [page, categoryFilters, relationTo, onResultChange, sort, limit, populateBy])
 
   return (
     <div className={[classes.collectionArchive, className].filter(Boolean).join(' ')}>
